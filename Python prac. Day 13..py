105c14bb85b60d82472928d6cb5af3c3b8a1f721
Durvish Raj Kumawat <durvishrajkumawat11@gmail.com>
2025-07-29 23:40:18 +0530

    Add files via upload

---
+++ Python prac. Day 13..py
@@ -0,0 +1,161 @@
+a="AbcDefFGHi"
+print(a.upper())
+
+a="AbcDEfghIJKlm"
+print(a.lower())
+
+b="     Raju lala baja bajay ga kala      "
+print(b.strip())
+
+c=" Kala  hai tu ??? !!!!"
+print(c.rstrip("!"))
+
+g="/Durvish Raj Kumawataaaaaaaaaaa//////////////"
+print(g.rstrip("a/"))
+
+h="Aarshi Bhatanager???????????? "
+print(h.rstrip("?"))
+
+j="Hey this side durvish"
+print(j.replace("durvish","Aarshi"))
+
+raja="This is best side of durvish"
+print(raja.replace("durvish","Aarshi"))
+
+raja="Introducing is python progarming language"
+print(raja.replace("is","to"))
+
+baba="Hemlo bro this side durvish raj kumawat"
+print(baba.split())
+
+ha="haha haha haha haha haha"
+print(ha.split())
+
+t="Hey I am raja babu, to Mumbai......."
+print(t.replace("to","from"))
+
+r="My fav. car is DODGE  CHALLENGER GRT HELLCAT, I buy this car in few year like under 2036 to 2040"
+print(r.replace("GRT","SRT"))
+
+o="Baba ne kaha ki khana hai to kha liya"
+print(o.split(","))
+
+o="Silver spoon"
+print(o.split("/"))
+
+
+
+str1="my self durvish raj kumawat"
+print(str1.capitalize())
+str2="raJa KAha BAchha hai hahahHAHAHHAhahahahHAHahhHhAh"
+print(str2.capitalize())
+
+t="Hey I am Durvish Raj Kumawat"
+print(t.center(515))
+
+tr="Hello bro"
+print(tr.center(100,"."))
+
+yu="Raju have a white cow and black donkey. "
+print(yu.count("a"))
+
+tu="My self durvish raj kumawat........"
+print(tu.endswith("."))
+
+p="yum yum meggie!!!"
+print (p.endswith("!"))
+
+pq="yum yum meggie!!!"
+print (pq.endswith("~"))
+
+b="I know that a one gyu is very bad gay....."
+print(b.endswith("a",10,20))
+
+str1 = "Welcome to the Console !!!"
+print(str1.endswith("to", 4, 10))
+
+ji=" hey a is a are a vhich a is bi*ch. "
+print(ji.count("a")) 
+
+ji2=" my gurl is very prety girl in entire world........."
+print(ji2.find("entire"))
+
+ju="hey brother for another mother......."
+print(ju.index("for"))
+
+ai="lala for a baba baacha they are very lil boy..........."
+print(ai.index("."))
+
+str3="WelcomeTo9orworld"
+print(str3.isalnum())
+
+str5="yosyouareaveryhandsomeguyButyouAreveryUglyGuy"  
+print(str5.isalnum())
+
+str6="HeyIAmHerryPotterFromHogwartsSchoolOfMagicAndWizardry"
+print(str6.isalpha())
+
+str7="hey you know that i am very good boy in the entire world"
+print(str7.islower())
+
+str7="lala hai Kala Haaahaaa"
+print(str7.islower())
+
+str8="We wish a very happy Diwali to all of you"
+print(str8.isprintable())
+
+str9="               "
+print(str9.isspace())
+
+str9="    "
+print(str9.isspace())
+
+str9=""
+print(str9.isspace())
+
+print("False aa geya.......\n")
+
+str10="Hey Harry Potter I Am Very Happy To See You Again......."
+print(str10.istitle())
+
+str10="Hey harry potter I am very happy to see you again......."
+print(str10.istitle())
+
+print("")
+
+str11="HEY BEOTHER I AM VERY TO SEE YOUR SISTER AGAIN......."
+print(str11.isupper())
+
+str11="HEY BEOTHER I AM VERY TO SEE YOuR SIStER AGAIN......."
+print(str11.isupper())
+
+print("")
+
+str12="Python is a very good programming language for a new programmer to learn."
+print(str12.startswith("Python"))
+
+str12="Python is a very good programming language for a new programmer to learn."
+print(str12.startswith("is"))
+
+print("")
+
+str13="Python is A very Good programming language FOR a New programmer to Learn."
+print(str13.swapcase())
+
+print("")
+
+str14="he's name is Danny. danny is an honest man."
+print(str14.title())
+
+str15="he's name is dan and dan is brothe of danny."
+print(str15.title())
+
+print("")
+print("")
+print("")
+
+
+
+print("This is the end of Day 13...........")
+
+print("Byeeeeeee...............")
